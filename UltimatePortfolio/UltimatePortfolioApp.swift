25a01deeca7366319146a2a0649af84fc6e00810
ianchen0419 <ianchen0419@gmail.com>
2023-07-18 15:23:47 +0800

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by 陳怡安 on 2023/7/18.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
